7d75cec8bfe1c1ea26a9e8eaee485a7e7674a5b7
kashitiz.sharma <kashitiz@signitysolutions.com>
2023-06-06 13:14:15 +0530

    commit 2 resumepro Model 2

---
+++ setup.sh
@@ -0,0 +1,21 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"yvannbarbotts@gmail.com\"\n\
+" > ~/.streamlit/config.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+\n\
+[theme]\n\
+base = \"light\"\n\
+primaryColor = \"#336699\"\n\
+backgroundColor = \"#F6F6F6\"\n\
+secondaryBackgroundColor = \"#FFFFFF\"\n\
+textColor = \"#333333\"\n\
+font = \"sans serif\"\n\
+" > ~/.streamlit/config.toml
