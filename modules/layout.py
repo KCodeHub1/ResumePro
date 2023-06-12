b1abcf68712391bd7eb97fce13f9ea8ed410d2fc
sajivsingh5 <sajiv@signitysolutions.com>
2023-06-12 10:34:48 +0530

    change layout

--- modules/layout.py
+++ modules/layout.py
@@ -9,6 +9,23 @@ class Layout:
         st.markdown(
             """
             <style>
+            .header-container {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                background-color: lightgray;
+                background-repeat: no-repeat;
+                padding: 20px;
+                margin-bottom: 20px;
+            }
+            .gen-container {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                background-repeat: no-repeat;
+                padding: 20px;
+                margin-bottom: 20px;
+            }
             .header {
                 text-align: center;
                 font-family: 'Arial', sans-serif;
@@ -18,8 +35,25 @@ class Layout:
             .sub-header {
                 text-align: center;
                 font-family: 'Arial', sans-serif;
-                color: #495057;
+                color: #4c1685;
                 margin-bottom: 20px;
+            }
+            .headline {
+                text-align: center;
+                color: #007bff;
+                font-size: 48px;
+                line-height: 1.0834933333;
+                font-weight: 600;
+                letter-spacing: -0.003em;
+                font-family: SF Pro Display,SF Pro Icons,Helvetica Neue,Helvetica,Arial,sans-serif;
+            }
+            .subhead {
+                text-align: center;
+                font-size: 16px;
+                line-height: 1.1904761905;
+                font-weight: 600;
+                letter-spacing: .011em;
+                font-family: SF Pro Display,SF Pro Icons,Helvetica Neue,Helvetica,Arial,sans-serif;
             }
             </style>
             """,
@@ -28,9 +62,10 @@ class Layout:
     
         st.markdown(
             """
-            <h1 class="header">ResumePro</h1>
-            <h3 class="sub-header">Get Resume Insights and Latest Jobs To Apply</h3>
-            <h3 class="sub-header">Please upload a resume in PDF format to continue</h3>
+            <div class="gen-container">
+                <h1 class="headline">ResumePro</h1>
+                <h3 class="subhead">Get Resume Insights and Latest Jobs To Apply</h3>     
+            </div>
             """,
             unsafe_allow_html=True,
         )
@@ -42,8 +77,12 @@ class Layout:
         """
         st.markdown(
             """
-            <div style='text-align: center;'>
-                <h4>Enter your <a href="https://platform.openai.com/account/api-keys" target="_blank">OpenAI API key</a> to start chatting</h4>
+            <div class="header-container">
+                <div style='text-align: center;'>
+                    <h4 class="subhead">How to use ResumePro?</h3>
+                    <h3 class="subhead">Step 1: Enter your <a href="https://platform.openai.com/account/api-keys" target="_blank">OpenAI API key</a> to start chatting</h4>
+                    <h3 class="subhead">Step 2: Upload a resume in PDF format to continue</h3>
+                </div>
             </div>
             """,
             unsafe_allow_html=True,
@@ -63,6 +102,7 @@ class Layout:
                 color: #495057;
                 border: none;
                 box-shadow: none;
+                width: 100%;
             }
             .stTextInput input:focus {
                 background-color: #e9ecef;
@@ -88,12 +128,10 @@ class Layout:
             user_input = st.text_area(
                 "Query:",
                 key="input",
-                help="Enter your query here"
+                help="Enter your query here",
+                height=150
             )
             submit_button = st.form_submit_button(label="Send")
 
             is_ready = submit_button and user_input
         return is_ready, user_input
-
-
-    
